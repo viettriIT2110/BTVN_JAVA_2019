227743631d3bc32b0a971af59d5e4d8dd926bbdf
tridungqhk3 <tridung2721@gmail.com>
2019-10-10 23:24:22 +0700

    bai3

---
+++ week 2/bai3/Baitap.java
@@ -0,0 +1,15 @@
+package baitap;
+//Nhập 1 số nguyên n và một số thực m từ bàn phím và hiển thị giá trị vừa nhập ra màn hình.
+import java.util.Scanner;
+
+public class Baitap {
+    
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+        float m = cin.nextFloat();
+        System.out.println("Số n = " + n);
+        System.out.println("Số m = " + m);
+    }
+    
+}
