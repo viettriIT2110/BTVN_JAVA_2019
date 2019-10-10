3da7acbdaedb68cb06069294ddf4eabb1a42c71c
tridungqhk3 <tridung2721@gmail.com>
2019-10-10 22:58:14 +0700

    bai1

---
+++ week 2/bai1/Baitap.java
@@ -0,0 +1,9 @@
+package baitap;
+//In ra màn hình dòng chữ "Hello, this is my first application"
+public class Baitap {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, this is my first application");
+    }
+    
+}
